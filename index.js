be89d7d9fceb71a0f38cd64cc495a35a3fa7b71c
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-05 23:25:27 +0600

    now find one by id

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 
 // middlewar
@@ -33,7 +33,12 @@ async function run() {
         res.send(result)
     })
 
-
+    app.get("/services/:id", async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await serviceCollection.findOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

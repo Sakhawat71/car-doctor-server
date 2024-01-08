8d9b1da535058911ad2a8f8b79792e45290ae5c3
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-09 04:56:26 +0600

    delete one booking service

--- index.js
+++ index.js
@@ -57,6 +57,12 @@ async function run() {
         const result = await bookingCollection.insertOne(booking);
         res.send(result);
     })
+    app.delete('/bookings/:id', async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id : new ObjectId(id)};
+        const result = await bookingCollection.deleteOne(query);
+        res.send(result);
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });

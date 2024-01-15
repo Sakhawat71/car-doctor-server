cf5e6a6f069ae2659404f6f52b6ab2e5df886214
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-16 00:58:53 +0600

    everything rebuild

--- index.js
+++ index.js
@@ -17,7 +17,6 @@ app.use(cookieParser())
 
 // custom middlewar
 
-
 // const logger = async (req, res, next) => {
 //     console.log('colled form: ', req.host, req.originalUrl)
 //     next()
@@ -49,7 +48,22 @@ app.use(cookieParser())
 // };
 
 const verifyToken = async (req, res, next) => {
-    const token = req.cookies;
+    const token = req.cookies?.token;
+    
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized assess' })
+    }
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        
+        req.user = decoded;
+        next();
+    })
+
 }
 
 
@@ -73,28 +87,12 @@ async function run() {
 
 
         // auth releted api
-        // app.post('/jwt', async (req, res) => {
 
-        //     const user = req.body;
-        //     const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
-        //         expiresIn: '2d'
-        //     });
-
-        //     res
-        //         .cookie('token', token, {
-        //             httpOnly: true,
-        //             secure: false,
-        //             // sameSite: 'none'
-        //             //dont use same site : if you use sameSite , con't get token in cookies
-        //         })
-        //         .send({ success: true })
-        // });
         app.post('/jwt', async (req, res) => {
             const userEmail = req.body;
-            console.log(userEmail)
+            // console.log(userEmail)
             const token = jwt.sign(userEmail, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '10h' })
             // console.log(token)
-
             res
                 .cookie('token', token, {
                     httpOnly: true,
@@ -105,9 +103,7 @@ async function run() {
 
         app.post('/logout', async (req, res) => {
             const user = req.body;
-            
-
-            res.clearCookie('token', {maxAge: 0}).send({success: true})
+            res.clearCookie('token', { maxAge: 0 }).send({ success: true })
         })
 
         // services
@@ -131,21 +127,16 @@ async function run() {
 
         // booking service
 
-        app.get('/bookings', async (req, res) => {
+        app.get('/bookings', verifyToken, async (req, res) => {
             let query = {};
+
+            if(req.query.email !== req.user?.email){
+                return res.status(403).send({message: 'Forbidden Access'})
+            }
 
             if (req.query?.email) {
                 query = { email: req.query.email }
             }
-
-            // if (req.query?.email !== req.user.email) {
-            //     return res.status(403).send({ message: "Forbidden" })
-            // }
-
-            // console.log(req.query?.email)
-            // console.log(req.user.email)
-            // console.log('we get decoded data in bookings :'  ,req.user)
-            // console.log('get cookies', req.cookies.token)
 
             const result = await bookingCollection.find(query).toArray();
             res.send(result);

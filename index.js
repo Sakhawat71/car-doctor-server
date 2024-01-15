eec93ca54b2a468f4ad355f916e4edc8154ed418
sakhawat71 <sakhawat.showrav@gmail.com>
2024-01-16 05:29:25 +0600

    bug #4

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const port = process.env.PORT || 5000;
 // middlewar
 app.use(cors({
     origin: [
-        // 'http://localhost:5173',
+        'http://localhost:5173',
         'https://car-doctor-71.web.app'
     ],
     credentials: true,

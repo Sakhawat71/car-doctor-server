77361726a3129926655ff1e5f5616bf94016fecf
sakhawat71 <sakhawat.showrav@gmail.com>
2024-02-09 01:17:17 +0600

    secure: true

--- index.js
+++ index.js
@@ -76,7 +76,7 @@ async function run() {
             res
                 .cookie('token', token, {
                     httpOnly: true,
-                    secure: false,
+                    secure: true,
                     sameSite: 'none'
                 })
                 .send({ success: true })
